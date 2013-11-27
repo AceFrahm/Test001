5bfbb49ecc90fa4e34c0d9de17de9e63d6e3e216
AceFrahm <ace.frahm+GitHub@gmail.com>
2013-11-26 18:49:43 -0700

    Try PhoneGap001

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\phonegap_projects\test001\platforms\android\gen\in\spellcast\Test001\R.java \
+ : C:\phonegap_projects\test001\platforms\android\res\drawable\icon.png \
+C:\phonegap_projects\test001\platforms\android\res\drawable-hdpi\icon.png \
+C:\phonegap_projects\test001\platforms\android\res\drawable-ldpi\icon.png \
+C:\phonegap_projects\test001\platforms\android\res\drawable-mdpi\icon.png \
+C:\phonegap_projects\test001\platforms\android\res\drawable-xhdpi\icon.png \
+C:\phonegap_projects\test001\platforms\android\res\values\strings.xml \
+C:\phonegap_projects\test001\platforms\android\res\xml\config.xml \
+C:\phonegap_projects\test001\platforms\android\bin\AndroidManifest.xml \
